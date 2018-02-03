2f993250c75d86f2ee0392b1769c764eaa845447
Patrick Gauvin <gauvin.patrick@gmail.com>
2018-02-03 16:12:56 -0500

    request support 0 point

--- server/test/sql/requests.sql
+++ server/test/sql/requests.sql
@@ -1,20 +1,28 @@
 SELECT people.id, people.name,
   CASE WHEN sex = 0 THEN "M" ELSE "F" END AS sex,
   category_id, categories.name AS category,
-  (SELECT COUNT(datetime)
-   FROM rewards
-   WHERE people.id = person_id
-   GROUP BY person_id) AS points,
+  CASE WHEN (SELECT COUNT(*)
+    FROM rewards
+    WHERE people.id = person_id
+    GROUP BY person_id) IS NULL THEN 0 
+  ELSE (SELECT COUNT(*)
+    FROM rewards
+    WHERE people.id = person_id
+    GROUP BY person_id) END AS points,
   (SELECT CASE WHEN sex = 0 THEN male_title ELSE female_title END AS title
    FROM titles
    WHERE people.category_id = titles.category_id AND
          cost = (SELECT MAX(cost)
                  FROM titles
                  WHERE people.category_id = titles.category_id AND 
-                       cost <= (SELECT COUNT(datetime)
-                                FROM rewards
-                                WHERE people.id = person_id
-                                GROUP BY person_id)
+                       cost <= CASE WHEN (SELECT COUNT(*)
+                                 FROM rewards
+                                 WHERE people.id = person_id
+                                 GROUP BY person_id) IS NULL 
+                               THEN 0 ELSE (SELECT COUNT(*)
+                                 FROM rewards
+                                 WHERE people.id = person_id
+                                 GROUP BY person_id) END
     GROUP BY titles.category_id)) AS title
 FROM people
 LEFT JOIN categories ON categories.id = category_id
