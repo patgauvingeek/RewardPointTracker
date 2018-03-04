c0acf159454e6c4f841dfe1fcf2418fd957b8d02
Patrick Gauvin <gauvin.patrick@gmail.com>
2018-03-04 10:27:12 -0500

    allow delete method

--- server/app/config/worker-config.js
+++ server/app/config/worker-config.js
@@ -17,7 +17,7 @@ function configureApplication(application) {
 
   application.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", " http://localhost:9001");
-    res.header("Access-Control-Allow-Methods", "GET, PUT");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.set('Cache-Control', 'no-cache, no-store, must-revalidate');
     res.set('Pragma', 'no-cache');
