a189910c0ef62526f94c448913192e85f9ca2807
Patrick Gauvin <gauvin.patrick@gmail.com>
2018-03-06 17:21:19 -0500

    new setting for databaseFilename

--- server/app/rewards.js
+++ server/app/rewards.js
@@ -1,9 +1,10 @@
 var sqlite3 = require("sqlite3").verbose();
+var settingsConfig = require("./config/settings/settings-config");
 
 var Database = function () {};
 
 Database.prototype.connect = function (runQueries) {
-  db = new sqlite3.Database("./rewards.db");
+  db = new sqlite3.Database(settingsConfig.settings.databaseFilename);
   // makes sure the foreign keys are activated.
   db.run("PRAGMA foreign_keys = ON;", err => {
     if (err)
